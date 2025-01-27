1c416215229e0ea77cd298fb969d19854dc19f1b
Rashi <rashi1704baranwal@gmail.com>
2025-01-27 11:25:15 +0530

    feat: react ai chat image recognition and chat streaming

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -6,33 +6,63 @@ import model from "../../lib/gemini";
 import Markdown from "react-markdown";
 
 const NewPrompt = () => {
-  const [question,setQuestion] = useState("");
-  const [answer,setAnswer] = useState("");
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
   const [img, setImg] = useState({
     isLoading: false,
     error: "",
     dbData: {},
+    aiData: {},
+  });
+
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+    generationConfig:{
+
+    },
   });
 
   const endRef = useRef(null);
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
-  }, [question,answer,img.dbData]);
+  }, [question, answer, img.dbData]);
 
   const add = async (text) => {
     setQuestion(text);
 
-    const result = await model.generateContent(text);
-    const response = await result.response;
-    setAnswer(response.text());
+    const result = await chat.sendMessageStream(
+      Object.entries(img.aiData).length ? [img.aiData, text] : [text]
+    );
+    let accumulatedText = "";
+    for await(const chunk of result.stream){
+      const chunkText = chunk.text();
+      console.log(chunkText);
+      accumulatedText += chunkText;
+      setAnswer(accumulatedText);
+    }
+    setImg({
+      isLoading: false,
+      error: "",
+      dbData: {},
+      aiData: {},
+    });
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const text = e.target.text.value;
-    if(!text) return;
+    if (!text) return;
     add(text);
   };
 
@@ -45,11 +75,15 @@ const NewPrompt = () => {
           urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
           path={img.dbData?.filePath}
           width="380"
-          transformation={[{width:380}]}
+          transformation={[{ width: 380 }]}
         />
       )}
       {question && <div className="message user">{question}</div>}
-      {answer && <div className="message"><Markdown>{answer}</Markdown></div>}
+      {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
       <div className="endChat" ref={endRef}></div>
       <form className="newForm" onSubmit={handleSubmit}>
         <Upload setImg={setImg} />
--- client/src/components/upload/Upload.jsx
+++ client/src/components/upload/Upload.jsx
@@ -39,8 +39,22 @@ const Upload = ({ setImg }) => {
   };
 
   const onUploadStart = (evt) => {
-    console.log("Start", evt);
-    setImg((prev) => ({ ...prev, isLoading: true }));
+    const file = evt.target.files[0];
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImg((prev) => ({
+        ...prev,
+        isLoading: true,
+        aiData: {
+          inlineData: {
+            data: reader.result.split(",")[1],
+            mimeType: file.type,
+          },
+        },
+      }));
+    };
+    reader.readAsDataURL(file);
   };
   return (
     <IKContext
@@ -59,7 +73,7 @@ const Upload = ({ setImg }) => {
         ref={ikUploadRef}
       />
       {
-        <label onClick={()=>ikUploadRef.current.click()}>
+        <label onClick={() => ikUploadRef.current.click()}>
           <img src="/attachment.png" alt="" />
         </label>
       }
